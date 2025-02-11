fb5e64546a9195b4b9d79069374901f2ddff7346
Paul Beaton <98857333+pbton@users.noreply.github.com>
2025-02-11 00:12:21 +0000

    experiment tracking

--- src/train.py
+++ src/train.py
@@ -54,5 +54,38 @@ def train(data_path,model_path,random_state,n_estimators,max_depth):
 
         ## predict and evaluate the model
         y_pred=best_model.predict(X_test)
-        accuracy_score=accuracy_score(y_test,y_pred)
-        print(f"Accuracy:{accuracy_score}")
+        accuracy=accuracy_score(y_test,y_pred)
+        print(f"Accuracy:{accuracy}")
+
+        ## Log additional metrics
+        mlflow.log_metric("accuracy",accuracy)
+        mlflow.log_param("best_n_estimators",grid_search.best_params_['n_estimators'])
+        mlflow.log_param("best_max_depth",grid_search.best_params_['max_depth'])
+        mlflow.log_param("best_sample_split",grid_search.best_params_['min_samples_split'])
+        mlflow.log_param("best_samples_leaf",grid_search.best_params_['min_samples_leaf'])
+
+        ## log the confusion matrix and classification report
+
+        cm=confusion_matrix(y_test,y_pred)
+        cr=classification_report(y_test,y_pred)
+
+        mlflow.log_text(str(cm),"confusion_matrix.txt")
+        mlflow.log_text(cr,"classification_report.txt")
+
+        tracking_url_type_store=urlparse(mlflow.get_tracking_uri()).scheme
+
+        if tracking_url_type_store!='file':
+            mlflow.sklearn.log_model(best_model,"model",registered_model_name="Best Model")
+        else:
+            mlflow.sklearn.log_model(best_model,"model",signature=signature)
+
+        ## create the directory to save the model
+        os.makedirs(os.path.dirname(model_path),exist_ok=True)
+
+        filename=model_path
+        pickle.dump(best_model,open(filename,'wb'))
+
+        print(f"Model saved to {model_path}")
+
+if __name__=="__main__":
+    train(params['data'],params['model'],params['random_state'],params['n_estimators'],params['max_depth'])
